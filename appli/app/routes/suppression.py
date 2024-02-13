53c4e0927347672808dc11a51a1f61209bb568f2
gromettoclara <129292328+gromettoclara@users.noreply.github.com>
2024-02-13 12:27:30 +0100

    conteneur et route supp
    route supp perdue

--- appli/app/routes/suppression.py
+++ appli/app/routes/suppression.py
@@ -3,14 +3,18 @@ from flask import render_template, request, redirect, url_for, flash
 from ..models.data import Maisons, Personnes
 from ..utils.transformations import  clean_arg
 
-@app.route("/suppression/pays", methods=['GET', 'POST'])
-def sup_maison():
+#la route qui supprime l
+
+@app.route("/suppression/maisons/<string:nom_maison>", methods=['GET', 'POST'])
+def sup_maison(nom_maison):
 
     try:
         if request.method == 'POST':
             nom_maison_a_supp = request.form.get('maison_a_supp')
             maison_a_supp = Maisons.query.filter(denomination=nom_maison_a_supp)
+            personne_a_supp = Personnes.query.filter(idWikidata=maison_a_supp.idWikidata)
             db.session.delete(maison_a_supp)
+            db.session.delete(personne_a_supp)
             db.session.commit()
             flash('La maison a été supprimée avec succès.', 'success')
 
