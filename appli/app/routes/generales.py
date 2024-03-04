6a242d8afca6e99a1d30a8cdbbae531ee81f1ea6
MaddalenaA <madda.accardo@gmail.com>
2024-03-04 23:40:55 +0100

    ajout du 2e graphique, essai 2

--- appli/app/routes/generales.py
+++ appli/app/routes/generales.py
@@ -72,13 +72,11 @@ def carte():
         donnees = donnees)
 
 #En cours d'adaptation pour les graphiques
-@app.route("/graphiques.html", methods=['GET'])
-def graphiques():
-    return render_template("/graphique1.html")
-
-@app.route("/graphiques", methods=['GET'])
-def graphiques_general():
-   return("rien pour le moment") #suggestion : faire un template page générale renvoyant par un lien à chaque graphique ? (Pauline)
+@app.route("/graphiques/graph1", methods=['GET', 'POST'])
+def graphiques_domaines():
+    types_count = db.session.query(Maisons.type, db.func.count(Maisons.id)).group_by(Maisons.type).all()
+    labels = [result[0].value if result[0] is not None else 'NULL' for result in types_count]
+    return render_template('pages/graphiques.html')
 
 #Graphique concernant les domaines des maisons des illustres : 
 @app.route("/graphiques/domaines", methods=['GET', 'POST'])
@@ -87,26 +85,3 @@ def graphiques_domaines():
     labels = [result[0].value if result[0] is not None else 'NULL' for result in types_count]
     counts = [result[1] for result in types_count]
     return render_template('pages/graphiques_domaines.html', labels=labels, counts=counts)
-
-#Aides
-"""@app.route("/graphiques/ressources_pays", methods=['GET', 'POST'])
-def graphiques_ressources_pays():
-    return render_template("pages/graphiques/ressources_pays.html")
-
-@app.route("/graphiques/ressources_pays_donnees", methods=['GET', 'POST'])
-def graphiques_ressources_pays_donnees():
-    donnees_brutes = db.session.query(Country, func.count(country_resources.c.resource).label('total'))\
-        .join(country_resources, )\
-        .group_by(Country.id)\
-        .order_by(text('total DESC'))\
-        .limit(20)
-
-    donnees = []
-
-    for pays in donnees_brutes.all():
-        donnees.append({
-            "label": pays[0].name,
-            "nombre": pays.total
-        })
-
-    return donnees"""
